0f347efee24ce559622e981a9de7accea85c7b11
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2023-10-31 17:36:56 +0000

    update

--- apps/guilds/client/src/app/page.tsx
+++ apps/guilds/client/src/app/page.tsx
@@ -566,7 +566,7 @@ export default function Home() {
                 className="sr-only peer"
               />
               <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-1/2 after:-translate-y-1/2 after:left-4 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              <span className="ml-3 text-sm font-medium dark:text-gray-300">
                 Paid content?
               </span>
             </label>
@@ -649,7 +649,7 @@ export default function Home() {
                 className="sr-only peer"
               />
               <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-1/2 after:-translate-y-1/2 after:left-4 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              <span className="ml-3 text-sm font-medium dark:text-gray-300">
                 Paid content?
               </span>
             </label>
