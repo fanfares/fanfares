0bb2d11c84882d02665cf81bb29e54624151bf85
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2023-12-21 14:04:22 +0000

    update settings form page

---
+++ apps/fanfares/src/app/(routes)/settings/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+import Button from "@/app/components/Button"
+import LabelForm from "@/app/components/LabelForm"
+import { useState } from "react"
+
+interface InputState {
+  id: number
+  value: string
+  label: string
+  placeholder: string
+}
+
+function Settings() {
+  const initialInputs: InputState[] = [
+    { id: 1, value: "", label: "Username", placeholder: "@ Username" },
+    { id: 2, value: "", label: "Display Name", placeholder: "Your name" },
+    {
+      id: 3,
+      value: "",
+      label: "Lightning Address",
+      placeholder: "user@lightningaddress.com",
+    },
+    {
+      id: 4,
+      value: "",
+      label: "Verified Nostr Id",
+      placeholder: "user@nostrnip05.com",
+    },
+  ]
+  const [inputs, setInputs] = useState<InputState[]>(initialInputs)
+  const [textAreaInput, setTextAreaInput] = useState("")
+
+  const handleInputChange = (id: number, value: string) => {
+    const updatedInputs = inputs.map(input =>
+      input.id === id ? { ...input, value } : input
+    )
+    setInputs(updatedInputs)
+  }
+
+  return (
+    <div className="w-full flex flex-col gap-5 items-center ">
+      <div>
+        <img src="" alt="" />
+      </div>
+
+      {inputs.map(input => (
+        <LabelForm className="relative gap-2 group" key={input.id}>
+          <p
+            className={`text-xs font-thin absolute transition-all duration-300 ease-in-out w-full group-focus-within:-top-4 group-focus-within:text-buttonAccentHover 
+          ${input.value.length !== 0 ? "-top-4" : ""}`}>
+            {input.label}
+          </p>
+          <input
+            className={`bg-transparent focus:outline-none w-full group-focus-within:text-left group-focus-within:placeholder:text-left transition-all duration-300 ease-linear
+            ${
+              input.value.length !== 0
+                ? "placeholder:text-left text-left"
+                : "placeholder:text-right text-right"
+            }`}
+            type="text"
+            value={input.value}
+            placeholder={input.placeholder}
+            onChange={e => handleInputChange(input.id, e.target.value)}
+          />
+        </LabelForm>
+      ))}
+
+      <LabelForm className="relative gap-2 group">
+        <p
+          className={`text-xs font-thin absolute group-focus-within:-top-4 group-focus-within:text-buttonAccentHover transition-all duration-300 ease-in-out w-full
+            ${textAreaInput.length !== 0 ? "-top-4" : ""}`}>
+          About Me
+        </p>
+        <textarea
+          rows={12}
+          value={textAreaInput}
+          onChange={e => setTextAreaInput(e.target.value)}
+          className={`bg-transparent group-focus:outline-dashed focus:outline-none w-full placeholder:text-right resize-none`}
+        />
+      </LabelForm>
+
+      <div className="flex gap-2">
+        <Button label="Save" className="w-40" />
+        <Button label="Cancel" className="w-40" />
+      </div>
+    </div>
+  )
+}
+
+export default Settings
--- apps/fanfares/src/app/components/LabelForm.tsx
+++ apps/fanfares/src/app/components/LabelForm.tsx
@@ -3,16 +3,18 @@ import { StringDecoder } from "string_decoder"
 interface FormLabelCreatorsProps {
   children: React.ReactNode
   id?: string
+  className?: string
 }
 
 export default function FormLabelCreators({
   children,
+  className,
   id,
 }: FormLabelCreatorsProps) {
   return (
     <label
       id={id}
-      className="px-4 py-2 text-xs font-semibold text-start text-white rounded-lg md:max-w-md flex flex-col min-w-fit md:w-1/2 bg-skin-fill">
+      className={`px-4 py-2 text-xs font-semibold text-start text-white rounded-lg md:max-w-md flex flex-col min-w-fit md:w-1/2 bg-skin-fill ${className}`}>
       {children}
     </label>
   )
