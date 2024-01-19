039b77cebe6befa8adf2db5eab6f7a185395965f
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-01-19 17:18:34 +0000

    update

--- apps/fanfares/src/app/components/ProfileBubble.tsx
+++ apps/fanfares/src/app/components/ProfileBubble.tsx
@@ -3,20 +3,19 @@
 import { useAppState } from "../controllers/state/use-app-state"
 
 export function ProfileBuble() {
-  const { accountProfile, primalProfiles } = useAppState()
-  const profile = primalProfiles.pubkey
+  const { accountProfile } = useAppState()
 
   if (!accountProfile) return null
 
   return (
     <div className="flex bg-skin-fill rounded-full items-center w-36 mx-auto p-1 h-10 gap-1 hover:bg-skin-fill/70 transition-colors ease-linear cursor-pointer">
       <div className="w-8 bg-skin-fill h-8 rounded-full">
-        <img src={profile.picture} alt="" />
+        <img src={accountProfile.picture} alt="" />
       </div>
       <div className="flex flex-col items-start w-40">
-        <p className="text-xs truncate">{profile.name}</p>
+        <p className="text-xs truncate">{accountProfile.name}</p>
         <p className="text-xs text-skin-muted truncate w-4/5">
-          {profile.lud16}
+          {accountProfile.lud16}
         </p>
       </div>
     </div>
