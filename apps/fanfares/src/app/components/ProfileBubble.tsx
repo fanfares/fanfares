3b533614cb7d8e8cdbe21df5307217aea56b73d8
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-01-19 16:55:14 +0000

    update

--- apps/fanfares/src/app/components/ProfileBubble.tsx
+++ apps/fanfares/src/app/components/ProfileBubble.tsx
@@ -9,7 +9,7 @@ export function ProfileBuble() {
 
   return (
     <div className="flex bg-skin-fill rounded-full items-center w-36 mx-auto p-1 h-10 gap-1 hover:bg-skin-fill/70 transition-colors ease-linear cursor-pointer">
-      <div className="w-8 bg-red-500 h-8 rounded-full">
+      <div className="w-8 bg-skin-fill h-8 rounded-full">
         <img src={accountProfile.picture} alt="" />
       </div>
       <div className="flex flex-col items-start w-40">
