e3d3d83c0187b42bbc5448ba48ff9147bc529b6d
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-01-22 13:43:02 +0000

    update

--- apps/fanfares/src/app/components/ProfileBubble.tsx
+++ apps/fanfares/src/app/components/ProfileBubble.tsx
@@ -8,9 +8,9 @@ export function ProfileBuble() {
   // if (!accountProfile) return null
 
   return (
-    <div className="flex bg-skin-fill rounded-full items-center w-36 mx-auto p-1 h-10 gap-1 hover:bg-skin-fill/70 transition-colors ease-linear mb-4">
-      <div className="w-12 rounded-full flex items-center justify-center">
-        {/* {accountProfile?.picture ? (
+    <div className="flex bg-skin-fill rounded-full items-center w-36 mx-auto p-1 h-10 hover:bg-skin-fill/70 transition-colors ease-linear mb-4 gap-1">
+      <div className="w-8 rounded-full flex items-center justify-center">
+        {accountProfile?.picture ? (
           <img
             src={accountProfile?.picture}
             alt=""
@@ -18,18 +18,20 @@ export function ProfileBuble() {
           />
         ) : (
           "🎪"
-        )} */}
-        <img
-          src={accountProfile?.picture}
+        )}
+        {/* <img
+          src={
+            "https://shdw-drive.genesysgo.net/FR9UETvWqw4YrxnrjrxJDGpjrpYmcg1UTDiKF99XSAsS/debug_clayno.png"
+          }
           className="w-full rounded-full object-cover object-center"
-        />
+        /> */}
       </div>
-      <div className="flex flex-col items-start w-40">
-        <p className="text-xs truncate">
-          {accountProfile?.name || "Nostr User"}
+      <div className="flex flex-col items-start w-[100px] font-thin">
+        <p className="text-[0.7rem] truncate">
+          {accountProfile?.name || "Wemerson"}
         </p>
-        <p className="text-xs text-skin-muted truncate w-4/5">
-          {accountProfile?.lud16 || "Nostr Lud16"}
+        <p className="text-[0.6rem] font-  text-skin-muted truncate w-4/5 line-clamp-2">
+          {accountProfile?.lud16 || "wemerson@getalby.com"}
         </p>
       </div>
     </div>
