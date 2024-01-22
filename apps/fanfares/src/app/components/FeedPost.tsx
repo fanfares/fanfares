296af6324f8f2b473027bf891217cc30bf8b8927
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-01-22 14:20:47 +0000

    update

--- apps/fanfares/src/app/components/FeedPost.tsx
+++ apps/fanfares/src/app/components/FeedPost.tsx
@@ -16,7 +16,6 @@ interface FeedPostProps {
 
 export function FeedPost(props: FeedPostProps) {
   const { user, userPfp, content, userProfile, note, onClick } = props
-  const [zapModalOn, setZapModalOn] = useState(false)
   const [fanfaresButtonMessage, setFanfaresButtonMessage] = useState(false)
   const [zapButtonMessage, setZapButtonMessage] = useState(false)
 
@@ -24,7 +23,41 @@ export function FeedPost(props: FeedPostProps) {
     <div
       id="e2e-feed-post-container"
       className="border-buttonAccent w-full rounded-md flex relative border pl-16 pr-4 py-3 flex-col">
-      <Modal isOpen={zapModalOn}>
+      <div className="w-12 h-12 absolute left-2 top-2 group">
+        <img
+          src={userPfp ?? "http://placebeard.it/640/480.jpg"}
+          className="w-full h-full border-2 border-buttonAccent object-cover rounded-full group-hover:border-buttonAccentHover"
+          alt="Profile Image"
+        />
+      </div>
+      <div className="flex-grow overflow-hidden space-y-1">
+        <p className="text-sm font-bold">
+          {user}{" "}
+          <Link
+            href="#"
+            className="text-white/50 font-medium hover:text-buttonAccentHover">
+            {userProfile ? userProfile : null}
+          </Link>
+        </p>
+        <h3 className="break-words text-sm font-normal">{content}</h3>
+        {/* <h3 className="break-words text-sm font-normal">{JSON.stringify(note)}</h3> */}
+      </div>
+      <Modal isOpen={fanfaresButtonMessage}>
+        <div className="w-80 flex flex-col gap-4 p-2 ">
+          <div className="w-full flex justify-between items-center">
+            <p>Coming Soon</p>
+            <button
+              onClick={() => setFanfaresButtonMessage(!fanfaresButtonMessage)}>
+              X
+            </button>
+          </div>
+          <p className="text-sm text-center">
+            Thanks for showing interest in our platform, this feature is not
+            working yet, but it's coming soon!
+          </p>
+        </div>
+      </Modal>
+      <Modal isOpen={zapButtonMessage}>
         <div className="flex flex-col px-2 py-1 w-96 gap-y-5">
           <p className="font-bold text-lg"> Select Sats</p>
           <div className="flex w-full">
@@ -36,7 +69,7 @@ export function FeedPost(props: FeedPostProps) {
             </p>
             <button
               onClick={() => {
-                setZapModalOn(!zapModalOn)
+                setZapButtonMessage(!zapButtonMessage)
               }}
               className="ml-auto">
               Ⅹ
@@ -77,40 +110,7 @@ export function FeedPost(props: FeedPostProps) {
           <Button label="Confirm" className="mb-4" />
         </div>
       </Modal>
-      <Modal isOpen={fanfaresButtonMessage}>
-        <div className="w-80 flex flex-col gap-4 p-2 ">
-          <div className="w-full flex justify-between items-center">
-            <p>Coming Soon</p>
-            <button
-              onClick={() => setFanfaresButtonMessage(!fanfaresButtonMessage)}>
-              X
-            </button>
-          </div>
-          <p className="text-sm text-center">
-            Thanks for showing interest in our platform, this feature is not
-            working yet, but it's coming soon!
-          </p>
-        </div>
-      </Modal>
-      <div className="w-12 h-12 absolute left-2 top-2 group">
-        <img
-          src={userPfp ?? "http://placebeard.it/640/480.jpg"}
-          className="w-full h-full border-2 border-buttonAccent object-cover rounded-full group-hover:border-buttonAccentHover"
-          alt="Profile Image"
-        />
-      </div>
-      <div className="flex-grow overflow-hidden space-y-1">
-        <p className="text-sm font-bold">
-          {user}{" "}
-          <Link
-            href="#"
-            className="text-white/50 font-medium hover:text-buttonAccentHover">
-            {userProfile ? userProfile : null}
-          </Link>
-        </p>
-        <h3 className="break-words text-sm font-normal">{content}</h3>
-        {/* <h3 className="break-words text-sm font-normal">{JSON.stringify(note)}</h3> */}
-      </div>
+
       <div className="mt-5 mx-auto flex gap-4">
         <Button
           className="px-2 w-28"
