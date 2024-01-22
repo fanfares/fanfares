621109120536f05eb1e1d33b116d8864712ad042
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-01-22 11:17:29 +0000

    updatepill

--- apps/fanfares/src/app/components/ProfileBubble.tsx
+++ apps/fanfares/src/app/components/ProfileBubble.tsx
@@ -8,14 +8,20 @@ export function ProfileBuble() {
   if (!accountProfile) return null
 
   return (
-    <div className="flex bg-skin-fill rounded-full items-center w-36 mx-auto p-1 h-10 gap-1 hover:bg-skin-fill/70 transition-colors ease-linear cursor-pointer">
-      <div className="w-8 bg-skin-fill h-8 rounded-full">
-        <img src={accountProfile.picture && ""} alt="" />
+    <div className="flex bg-skin-fill rounded-full items-center w-36 mx-auto p-1 h-10 gap-1 hover:bg-skin-fill/70 transition-colors ease-linear cursor-pointer mb-4">
+      <div className="w-8 bg-skin-fill h-8 rounded-full flex items-center justify-center">
+        {accountProfile?.picture ? (
+          <img src={accountProfile?.picture} alt="" />
+        ) : (
+          "🎪"
+        )}
       </div>
       <div className="flex flex-col items-start w-40">
-        <p className="text-xs truncate">{accountProfile.name}</p>
+        <p className="text-xs truncate">
+          {accountProfile?.name || "Nostr User"}
+        </p>
         <p className="text-xs text-skin-muted truncate w-4/5">
-          {accountProfile.lud16}
+          {accountProfile?.lud16 || "Nostr Lud16"}
         </p>
       </div>
     </div>
