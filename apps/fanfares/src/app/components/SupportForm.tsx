50ba5f6af4fe358a3fa365574f65bd3fd144e0d3
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-01-29 10:40:20 +0000

    update

--- apps/fanfares/src/app/components/SupportForm.tsx
+++ apps/fanfares/src/app/components/SupportForm.tsx
@@ -2,53 +2,29 @@
 import { useState } from "react"
 import AnimatedLabelInput from "./AnimatedLabelInput"
 import Button from "./Button"
-import Link from "next/link"
 
-interface ContactFormProps {
-  feedback?: boolean
-  callToAction?: string
-  lud16?: string
-}
+import {
+  useSupportFormName,
+  useSupportFormEmail,
+  useSupportFormMessage,
+  useSupportFormLoading,
+  useSupportFormActions,
+} from "../controllers/state/support-form-slice"
+import { AnimatedLabelTextInput } from "./AnimatedLabelInputText"
+import { AnimatedLabelTextAreaInput } from "./AnimatedLabelInputTextArea"
 
-const ContactForm = (props: ContactFormProps) => {
-  const { feedback, callToAction, lud16 } = props
-  // TODO: Split this data to individual states
+const ContactForm = () => {
   //TODO : Loading icon on Submit
   // TODO: Support form and Feedback form
-  
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  })
+  const name = useSupportFormName()
+  const email = useSupportFormEmail()
+  const message = useSupportFormMessage()
+  const isLoading = useSupportFormLoading()
+  const { submit, setName, setEmail, setMessage } = useSupportFormActions()
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
-    try {
-      if (!formData.name) throw new Error("Name is required")
-      if (!formData.email) throw new Error("Email is required")
-      if (!formData.message) throw new Error("Message is required")
-
-      const res = await fetch("/api/support", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      })
-
-      if (res.ok) {
-        alert(
-          `Thanks for contacting us! We'll get back to you as soon as possible.`
-        )
-        setFormData({ name: "", email: "", message: "" })
-      } else {
-        alert("Error sending email.")
-      }
-    } catch (error) {
-      console.error(error)
-      alert(`Error sending email: ${error}`)
-    }
+    submit()
   }
 
   return (
@@ -57,61 +33,41 @@ const ContactForm = (props: ContactFormProps) => {
         <h1
           id="e2e-whitepaper-heading"
           className="mt-4 text-xl font-bold leading-10 md:text-2xl xl:text-3xl"></h1>
-        {feedback ? (
-          <>
-            <p className="mt-4 font-thin text-sm">
-              {" "}
-              We aim to perfect FanFares to be an intuitive and easy to use
-              application that gives access to Nostr for everyone no matter what
-              their level of technical abilty.
-            </p>
-            <p className="mt-4 font-thin text-sm">
-              For constructive feedback that genuinely adds value to the
-              application we are willing to pay our customers 10000 sats as a
-              reward for identifying problems and making suggestions as to how
-              the application can work better.
-            </p>
-          </>
-        ) : (
-          <p className="mt-4 font-thin text-sm">
-            We apologise if you are experiencing issues with FanFares. Please
-            write a description of your problem in the form below and we will
-            get back to you as soon as possible in order to help fix the
-            problem.
-            {/* If you need any help, want to give any feedback, or want to upload
-            your first audio, please reach out to us at:{" "}
-            <a className="underline" href="mailto:support@fanfares.io">
-              <p className="underline">support@fanfares.io</p>
-            </a> */}
-            {/* <p className="mt-2">
-            Alternatively you can send us a message using the form below.
-          </p>{" "} */}
-          </p>
-        )}
+
+        <p className="mt-4 font-thin text-sm">
+          We apologise if you are experiencing issues with FanFares. Please
+          write a description of your problem in the form below and we will get
+          back to you as soon as possible in order to help fix the problem.
+        </p>
+
         <form
           onSubmit={handleSubmit}
           className="flex flex-col items-start w-full gap-4 mt-8"
           action="">
-          <AnimatedLabelInput
-            onChange={input => setFormData({ ...formData, name: input })}
-            label={feedback ? "LUD16*" : "Name*"}
+          <AnimatedLabelTextInput
+            required={true}
+            label="Name*"
             htmlFor="name"
+            value={name}
+            onChange={setName}
           />
-          <AnimatedLabelInput
-            onChange={input => setFormData({ ...formData, email: input })}
+          <AnimatedLabelTextInput
+            required={true}
             label="Email*"
             htmlFor="email"
-            inputType={"email"}
+            value={email}
+            onChange={setEmail}
           />
-          <AnimatedLabelInput
-            onChange={input => setFormData({ ...formData, message: input })}
+          <AnimatedLabelTextAreaInput
+            required={true}
             label="Message*"
             htmlFor="message"
-            // messageField
+            value={message}
+            onChange={setMessage}
           />
           <Button
             type="submit"
-            label="Submit"
+            label={isLoading ? "Loading..." : "Submit"}
             className="flex items-center px-4 mt-8"
           />
         </form>
