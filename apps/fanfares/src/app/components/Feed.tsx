ee39209e0873b932f2faaf1875389be82a66a0c9
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-02-07 16:03:30 +0000

    update

--- apps/fanfares/src/app/components/Feed.tsx
+++ apps/fanfares/src/app/components/Feed.tsx
@@ -49,8 +49,7 @@ export function Feed() {
       <h1 className="font-black text-center text-gray-100 text-xl/4 md:mt-4 md:text-start md:text-4xl">
         Nostr Universe{" "}
       </h1>
-      {/* {primalIsFetching ? renderLoading() : renderNotes()} */}
-      {renderLoading()}
+      {primalIsFetching ? renderLoading() : renderNotes()}
     </>
   )
 }
