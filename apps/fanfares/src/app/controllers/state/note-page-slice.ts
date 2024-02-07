d41338357c86e9513c018387a9a944d776a3dc9f
Coach Chuck <christian.krueger.health@gmail.com>
2024-02-07 14:13:31 -0700

    working

--- apps/fanfares/src/app/controllers/state/note-page-slice.ts
+++ apps/fanfares/src/app/controllers/state/note-page-slice.ts
@@ -1,11 +1,29 @@
+import { Event as NostrEvent } from 'nostr-tools';
+import { SimplePool } from 'nostr-tools';
 import { StateCreator } from 'zustand';
 
 export interface NotePageSlice {
-    notePageState: string;
+    noteId: string | null;
+    note: NostrEvent | null;
+    noteFetching: boolean;
+    noteError: string | null;
+    actions: {
+        setNoteId: (
+            noteId: string,
+            nostrPool: SimplePool,
+            nostrRelays: string[]
+        ) => void;
+    }
 }
 
 const DEFAULT_STATE: NotePageSlice = {
-    notePageState: 'Test State',
+    noteId: null,
+    noteFetching: false,
+    note: null,
+    noteError: null,
+    actions: {
+        setNoteId: () => {},
+    }
 };
 
 export const createTestSlice: StateCreator<
@@ -14,8 +32,48 @@ export const createTestSlice: StateCreator<
   [],
   NotePageSlice
 > = (set, get) => {
+
+    const reset = () => {
+        set({ 
+            noteId: null,
+            note: null,
+            noteError: null,
+            noteFetching: false,
+        });
+    }
     
+    const setNoteId = (
+        noteId: string, 
+        nostrPool: SimplePool, 
+        nostrRelays: string[]
+    ) => {
+        reset();
+
+        set({ noteId });
+
+        // TODO start fetching
+        nostrPool.get(
+            nostrRelays,
+            {
+                ids: [noteId],
+            }
+        ).then((event) => {
+            if(event){
+                set({ note: event });
+            } else {
+                set({ noteError: "Note not found" });
+            }
+        }).finally(() => {
+            set({ noteFetching: false });
+        });
+
+    }
+
+
     return {
         ...DEFAULT_STATE,
+        actions: {
+            setNoteId,
+        }
     };
 };
