f0ef8f4e4f4e2e94a5cee6a20631ad9b3c544f0d
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-02-07 15:37:11 +0000

    lottie files working

--- apps/fanfares/src/app/(routes)/p/[id]/page.tsx
+++ apps/fanfares/src/app/(routes)/p/[id]/page.tsx
@@ -1,24 +1,34 @@
 "use client"
 
-import { usePrimalNoteStats, usePrimalNotes, usePrimalProfiles } from "@/app/controllers/state/primal-slice";
-import { getIdFromUrl } from "@/app/controllers/utils/formatting";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-
+import {
+  usePrimalNoteStats,
+  usePrimalNotes,
+  usePrimalProfiles,
+} from "@/app/controllers/state/primal-slice"
+import { getIdFromUrl } from "@/app/controllers/utils/formatting"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react"
+import Lottie from "lottie-react"
+import lottie from "./lottie.json"
 
 export default function Page() {
-    const primalNotes = usePrimalNotes();
-    const primalProfiles = usePrimalProfiles();
-    const primalNoteStats = usePrimalNoteStats();
-    const id = getIdFromUrl(usePathname())
+  const primalNotes = usePrimalNotes()
+  const primalProfiles = usePrimalProfiles()
+  const primalNoteStats = usePrimalNoteStats()
+  const id = getIdFromUrl(usePathname())
 
-    // ------------ USE EFFECTS ------------
-  
-    const profile = primalProfiles[id];
+  // ------------ USE EFFECTS ------------
 
-    if(!profile){
-        return <div>Profile not found</div>
-    }
+  const profile = primalProfiles[id]
 
-  return <div>Page of {profile.name}</div>;
+  if (!profile) {
+    return <div>Profile not found</div>
+  }
+
+  return (
+    <div>
+      <Lottie className="w-10" animationData={lottie} loop={true} />
+      Page of {profile.name}
+    </div>
+  )
 }
