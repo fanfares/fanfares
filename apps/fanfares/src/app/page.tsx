461ab2f76b43f5c5083647e3add4f70e5fb4e1cb
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-04-10 13:54:48 +0100

    update

--- apps/fanfares/src/app/page.tsx
+++ apps/fanfares/src/app/page.tsx
@@ -14,27 +14,26 @@ export default function Home() {
   //TODO make this the Discover page
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-2 ">Welcome to FanFares</h1>
-      <p className="text-lg mb-6">
+      <h1 className="text-4xl font-bold">Welcome to FanFares</h1>
+      <p className="text-base/5 mb-6">
         A premier Nostr client optimized for podcast monetization and curation.
       </p>
-      <div className=" shadow-lg p-6 mb-6">
+      <div className="p-6">
         <p className="text-lg ">
           Bitcoin Lightning is the breakthrough micropayment technology many
           have eagerly awaited for decades, and now it's finally here. At
           FanFares, we're harnessing this technology to liberate creators and
           their audiences from the influence of advertisers.
         </p>
-        <p className="text-lg  mt-4">
+        <p className="text-lg mt-4">
           The internet has long been a catalyst for disintermediation of systems
           where middlemen have control and siphon off the majority of the value
           created. With Bitcoin Lightning and Nostr, we're taking another stride
           towards empowering peer-to-peer communication and the exchange of
           value.
         </p>
-      </div>
-      <div className=" shadow-lg p-6 mb-6">
-        <p className="text-lg ">
+
+        <p className="text-lg mt-4 ">
           We understand human nature, and it is a true fact that most people are
           basically lazy. We all tend to take the most convenient route wherever
           possible. While we all believe in compensating creators for their
