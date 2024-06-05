8f94a4f36ec094a07d4b33129e96c62eadebbae8
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2024-06-05 10:09:10 +0100

    finished accordion

--- apps/fanfares/src/app/components/HomePageAccordion.tsx
+++ apps/fanfares/src/app/components/HomePageAccordion.tsx
@@ -37,7 +37,7 @@ function Box(props: HomePageAccordionProps) {
   const renderArrows = () => {
     return (
       <button
-        className={clsx("svgUp transition duration-300", {
+        className={clsx("svgUp transition duration-300 mt-3", {
           "rotate-180": isOpen,
           "rotate-0": !isOpen,
           "opacity-0": isOpening,
@@ -67,27 +67,28 @@ function Box(props: HomePageAccordionProps) {
 
   const renderDescription = () => {
     return (
-      <div
+      <p
         ref={descriptionRef}
         className={clsx(
           "overflow-hidden leading-normal text-white transition-all duration-500 ease-in-out",
           {
-            "line-clamp-2": !isOpen,
+            "line-clamp-1 h-6": !isOpen,
             "line-clamp-none": isOpen,
           }
         )}>
         {children}
-      </div>
+      </p>
     )
   }
 
   useEffect(() => {
-    if (isOpen && descriptionRef.current) {
-      const actualHeight = descriptionRef.current.scrollHeight || 0
-      descriptionRef.current.style.height = `${actualHeight}px`
-    } else if (descriptionRef.current) {
-      descriptionRef.current.style.height = "0"
-    }
+    if (!descriptionRef.current) return
+    // if (isOpen) {
+    //   const actualHeight = descriptionRef.current.scrollHeight || 0
+    //   descriptionRef.current.style.height = `${actualHeight}px`
+    // } else if (descriptionRef.current) {
+    //   descriptionRef.current.style.height = "0"
+    // }
     const timeout = setTimeout(() => setIsOpening(false), 500)
     return () => clearTimeout(timeout)
   }, [isOpen])
@@ -97,19 +98,18 @@ function Box(props: HomePageAccordionProps) {
       id={boxButtonId}
       onClick={setBox}
       className={clsx(
-        "mt-2  w-full cursor-pointer rounded  border border-gray-50/10 p-4 shadow drop-shadow-2xl filter backdrop-blur-md transition-all duration-500 ",
+        "mt-2 w-full cursor-pointer rounded flex border border-gray-50/10 p-4 shadow drop-shadow-2xl filter backdrop-blur-md transition-all duration-500 items-start min-h-[2rem]",
         {
           "bg-white/[5%] h-full": isOpen,
-          "bg-white/[2%] h-10": !isOpen,
+          "bg-white/[2%] h-fit": !isOpen,
         }
       )}>
       {renderDescription()}
-      <div
+      {renderArrows()}
+      {/* <div
         className={clsx(
           "arrow cursor-pointer ring-offset-white transition-opacity duration-500 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
-        )}>
-        {renderArrows()}
-      </div>
+        )}></div> */}
     </div>
   )
 }
@@ -117,7 +117,7 @@ function Box(props: HomePageAccordionProps) {
 const SectionTitle = (props: { title: string }) => {
   return (
     <>
-      <h2 className="font-font1 mx-auto w-40 text-center text-2xl font-black uppercase drop-shadow-2xl md:w-full md:text-center md:text-5xl">
+      <h2 className="font-font1 mx-auto w-40 text-center text-2xl font-black drop-shadow-2xl md:w-full md:text-center md:text-5xl mb-4">
         {props.title}
       </h2>
     </>
@@ -128,57 +128,55 @@ export default function HomePageAccordion() {
   const [boxState, setBoxState] = useState<number | null>(null)
 
   return (
-    <div className="relative flex flex-col items-center content-center justify-center w-full px-8 mx-auto mt-24">
+    <div className="relative flex flex-col items-center content-center justify-center w-full  mx-auto mt-16">
       {/* <span aria-disabled className="z-0 faq-svg-bl" />
       <span aria-disabled className="z-0 faq-svg-tr" /> */}
-      <div className="w-full mx-auto ">
-        <SectionTitle title="About Us" />
-      </div>
-      <div className="w-full lg:w-3/4">
-        <Box
-          boxTitleId="e2e-how-it-works-heading"
-          boxButtonId="e2e-how-it-works-box"
-          boxNumber={0}
-          setBoxState={setBoxState}
-          boxState={boxState}>
-          <p
-            id="e2e-how-it-works-text"
-            className="block mt-2 transition-all duration-1000">
-            Bitcoin Lightning is the breakthrough micropayment technology many
-            have eagerly awaited for decades, and now it's finally here. At
-            FanFares, we're harnessing this technology to liberate creators and
-            their audiences from the influence of advertisers.
-          </p>
-        </Box>
-        <Box boxNumber={1} setBoxState={setBoxState} boxState={boxState}>
-          <p className="block mt-2">
-            The internet has long been a catalyst for disintermediation of
-            systems where middlemen have control and siphon off the majority of
-            the value created. With Bitcoin Lightning and Nostr, we're taking
-            another stride towards empowering peer-to-peer communication and the
-            exchange of value.
-          </p>
-        </Box>
-        <Box boxNumber={2} setBoxState={setBoxState} boxState={boxState}>
-          <p className="block mt-2">
-            We understand human nature, and it is a true fact that most people
-            are basically lazy. We all tend to take the most convenient route
-            wherever possible. While we all believe in compensating creators for
-            their efforts, in a world accustomed to free access, many simply
-            don't pay unless prompted.
-          </p>
-        </Box>
 
-        <Box boxNumber={3} setBoxState={setBoxState} boxState={boxState}>
-          <p className="">
-            On FanFares, the creators of a podcast can set a fee for accessing a
-            podcast. Listeners get to enjoy every episode without being
-            interrupted with ads. They get back those few minutes of their
-            lives, which is worth the value of the sats to most of us because
-            our time is even more scarce than bitcoin!
-          </p>{" "}
-        </Box>
-      </div>
+      <SectionTitle title="About Us" />
+
+      <Box
+        boxTitleId="e2e-how-it-works-heading"
+        boxButtonId="e2e-how-it-works-box"
+        boxNumber={0}
+        setBoxState={setBoxState}
+        boxState={boxState}>
+        <p
+          id="e2e-how-it-works-text"
+          className="block transition-all duration-1000">
+          Bitcoin Lightning is the breakthrough micropayment technology many
+          have eagerly awaited for decades, and now it's finally here. At
+          FanFares, we're harnessing this technology to liberate creators and
+          their audiences from the influence of advertisers.
+        </p>
+      </Box>
+      <Box boxNumber={1} setBoxState={setBoxState} boxState={boxState}>
+        <p className="block transition-all duration-1000">
+          The internet has long been a catalyst for disintermediation of systems
+          where middlemen have control and siphon off the majority of the value
+          created. With Bitcoin Lightning and Nostr, we're taking another stride
+          towards empowering peer-to-peer communication and the exchange of
+          value.
+        </p>
+      </Box>
+      <Box boxNumber={2} setBoxState={setBoxState} boxState={boxState}>
+        <p className="block transition-all duration-1000">
+          We understand human nature, and it is a true fact that most people are
+          basically lazy. We all tend to take the most convenient route wherever
+          possible. While we all believe in compensating creators for their
+          efforts, in a world accustomed to free access, many simply don't pay
+          unless prompted.
+        </p>
+      </Box>
+
+      <Box boxNumber={3} setBoxState={setBoxState} boxState={boxState}>
+        <p className="block transition-all duration-1000">
+          On FanFares, the creators of a podcast can set a fee for accessing a
+          podcast. Listeners get to enjoy every episode without being
+          interrupted with ads. They get back those few minutes of their lives,
+          which is worth the value of the sats to most of us because our time is
+          even more scarce than bitcoin!
+        </p>{" "}
+      </Box>
     </div>
   )
 }
--- apps/fanfares/src/app/page.tsx
+++ apps/fanfares/src/app/page.tsx
@@ -3,6 +3,8 @@ import Button from "./components/Button"
 import router from "next/router"
 import Link from "next/link"
 import HomePageAccordion from "./components/HomePageAccordion"
+import Image from "next/image"
+import Logo from "./assets/logo.svg"
 
 export default function Home() {
   // ------------------- STATES -------------------------
@@ -18,25 +20,38 @@ export default function Home() {
   //TODO make this the Discover page
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold">FanFares</h1>
-      <p className="text-base/5 mb-6">
-        Welcome to the Internet-Money Era of Podcasting.
-      </p>
-      <div className="flex flex-col md:hidden gap-4">
+      <div className="flex items-center justify-center gap-4 ">
+        <Link href="/" className="flex flex-col items-center md:hidden">
+          <Image
+            className=""
+            width={70}
+            height={70}
+            src={Logo}
+            alt="FanFares Logo"
+          />
+        </Link>
+        <div className="flex-col">
+          <h1 className="text-4xl font-bold">FanFares</h1>
+          <p className="text-base/5">
+            Welcome to the Internet-Money Era of Podcasting.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col md:hidden gap-4 mt-8">
         <Link
           href="/upload"
-          className={`px-1 flex items-center border border-white/20 justify-center py-2 rounded-full transition-all duration-300 ease-in-out transform text-sm font-semibold hover:bg-skin-fill gap-2`}>
+          className={`px-1 flex items-center border border-white/20 justify-center py-2 rounded-md transition-all duration-300 ease-in-out transform text-sm font-semibold hover:bg-skin-fill gap-2`}>
           <span className="">ZapWall your podcast</span>
         </Link>
         <Link
           href="/discover"
-          className={`px-1 flex items-center border border-white/20 justify-center py-2 rounded-full transition-all duration-300 ease-in-out transform text-sm font-semibold hover:bg-skin-fill gap-2`}>
+          className={`px-1 flex items-center border border-white/20 justify-center py-2 rounded-md transition-all duration-300 ease-in-out transform text-sm font-semibold hover:bg-skin-fill gap-2`}>
           <span className="">Purchase ZapWalled podcasts</span>
         </Link>
       </div>
 
       <HomePageAccordion />
-      <div className="p-6 hidden md:block">
+      {/* <div className="p-6 hidden md:block">
         <p className="text-lg ">
           Bitcoin Lightning is the breakthrough micropayment technology many
           have eagerly awaited for decades, and now it's finally here. At
@@ -65,7 +80,7 @@ export default function Home() {
           which is worth the value of the sats to most of us because our time is
           even more scarce than bitcoin!
         </p>
-      </div>
+      </div> */}
     </div>
   )
 }
